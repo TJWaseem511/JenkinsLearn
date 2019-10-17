0868640989a323f6722173e38d61d3ed0c7b1c45
Waseem <tjwasim@gmail.com>
2019-10-17 12:46:31 +0530

    Formate Output

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 	public static void main(String args[]){
 		for(int i=1; i<=10; i++)
-		System.out.println("Hello World......"+i);
+		System.out.println(i+"....Hello World......");
 	}
 }
